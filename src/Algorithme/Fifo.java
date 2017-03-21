8debe0fd1dc81a74358d8a9bea24d1ebc00388df
chawki chouib <chouibc@gmail.com>
2017-03-21 22:18:49 +0100

    init

---
+++ src/Algorithme/Fifo.java
@@ -0,0 +1,7 @@
+package Algorithme;
+
+/**
+ * Created by chawki on 21/03/2017.
+ */
+public class Fifo {
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+
+/**
+ * Created by chawki on 21/03/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
