beabba8f0cbdad7057978c343b1a0b05d2cfbb75
ibtissem <ibtissemibtissem0@gmail.com>
2017-03-26 19:29:46 +0100

    variable pour priority et RR

--- src/Algorithme/Priority.java
+++ src/Algorithme/Priority.java
@@ -13,88 +13,86 @@ public class Priority {
         Scanner sc = new Scanner(System.in);
 
         int i = 0, n;
-        int apw1[];
+        int watingT1[];
 
         System.out.print("entrer le nombre de processus : ");
         n = sc.nextInt();
-        int bt[] = new int[n];
+        int timeCPU[] = new int[n];
         int n1 = n;
 
         //pr c'est la prioritée
-        int pr[] = new int[n];
+        int priority[] = new int[n];
 
         // pr1 la priority
-        int pr1[] = new int[n];
-
-        float t = 0;
-
+        int priority1[] = new int[n];
+        //temps globale
+        float time = 0;
         // temps pour sauvgarder une donnée
         int temp;
 
-        // le turnaround
-        int turn[] = new int[n];
+        int turnaround[] = new int[n];
         int p[] = new int[n];
-        float tu = 0;
+        float time2 = 0;
 
-        apw1 = new int[n + 1];
-        apw1[0] = 0;
+        watingT1 = new int[n + 1];
+        watingT1[0] = 0;
 
         for (i = 0; i < n; i++) {
-            System.out.println("ENTER burst time for each process: p" + (i + 1));
+            System.out.println("ENTER time CPU for each process: p" + (i + 1));
             // la lecture temps CPU
-            bt[i] = sc.nextInt();
+            timeCPU[i] = sc.nextInt();
         }
 
 // le lecture des priorité pour chaque processus
         for (i = 0; i < n; i++) {
             System.out.println("Enter the priority for p" + (i + 1));
-            pr[i] = sc.nextInt();
+            priority[i] = sc.nextInt();
         }
-// la copie des priorité dans pr1
+// la copie des priorité dans priority1
         for (i = 0; i < n; i++) {
-            pr1[i] = pr[i];
+            priority1[i] = priority[i];
         }
 // ordonner le tableau des prioritée du min au max( on considere que la petite priorité c'est  elle qui domine )
         for (i = 0; i < n; i++)
             for (int j = i + 1; j < n; j++)
-                if (pr1[i] > pr1[j]) {
-                    temp = pr1[i];
-                    pr1[i] = pr1[j];
-                    pr1[j] = temp;
+                if (priority1[i] > priority1[j]) {
+                    temp = priority1[i];
+                    priority1[i] = priority1[j];
+                    priority1[j] = temp;
                 }
 // comparer l'ancien tablaux avec le nouveux pour voir si on a des prioritée egaux
         for (i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
-                if (pr1[i] == pr[j])
+                if (priority1[i] == priority[j])
                     p[i] = j + 1;
 
         for (i = 0; i < n; i++) {
             int k = p[i];
-            apw1[i + 1] = bt[k - 1] + apw1[i];
+            watingT1[i + 1] = timeCPU[k - 1] + watingT1[i];
         }
 
         for (i = 0; i < n; i++) {
-            System.out.println("indivisual waiting time for process p" + p[i] + "is" + apw1[i] + " ");
+            System.out.println("indivisual waiting time for process p" + p[i]+" " + "is" +" " + watingT1[i] + " ");
         }
 
         for (i = 0; i < n; i++)
-            t += apw1[i];
-        float avg = t / n;
-        System.out.println("average waiting time is:" + avg);
+            time += watingT1[i];
+        float watingTimeMoyenne = time / n;
+        System.out.println("average waiting time is:" +" " + watingTimeMoyenne);
 
 
         for (i = 0; i < n; i++) {
             int k = p[i];
-            turn[i] = bt[k - 1] + apw1[i];
-            System.out.println("turnaround time for process p" + p[i] + "is" + turn[i] + " ");
+            turnaround[i] = timeCPU[k - 1] + watingT1[i];
+            System.out.println("turnaround time for process p" + p[i] +" "+ "is" +" "+ turnaround[i] + " ");
         }
 
 
         for (i = 0; i < n; i++)
-            tu += turn[i];
+            time2 += turnaround[i];
 
-        float avg1 = tu / n;
-        System.out.println("average turn-around time is:" + avg1);
+        float turnaroudMoyenne = time2 / n;
+        System.out.println("average turn-around time is:" +" " +turnaroudMoyenne);
 
 
     }
--- src/Algorithme/Roundrobin.java
+++ src/Algorithme/Roundrobin.java
@@ -9,29 +9,29 @@ import java.util.*;
 public class Roundrobin {
 
     Scanner sc = new Scanner(System.in);
-    int[] bur, rem, wai, ta;
-    int size, q, b = 0, t = 0, flag = 0;
+    int[] tempCPU, tempsCPU1, waitingT, teurnaround;
+    int size, quantum, b = 0, t = 0, flag = 0;
 
     public Roundrobin() {
     }
 
     Roundrobin(int size) {
         this.size = size;
-        bur = new int[size];
-        wai = new int[size];
-        ta = new int[size];
-        rem = new int[size];
+        tempCPU = new int[size];
+        waitingT = new int[size];
+        teurnaround = new int[size];
+        tempsCPU1 = new int[size];
     }
 
     // enter le le temps cpu pour chaque processus et le quantum
     void get() {
         for (int i = 0; i < size; i++) {
             System.out.print("Enter burst time of P" + (i + 1) + ":");
-            bur[i] = rem[i] = sc.nextInt();
+            tempCPU[i] = tempsCPU1[i] = sc.nextInt();
         }
 
         System.out.print("Enter quantum time:");
-        q = sc.nextInt();
+        quantum = sc.nextInt();
     }
 
     // calculer le turnaround et le waiting time pour chaque processus
@@ -42,44 +42,44 @@ public class Roundrobin {
         do {
             flag = 0;
             for (int i = 0; i < size; i++) {
-                if (rem[i] >= q) {
+                if (tempsCPU1[i] >= quantum) {
                     System.out.print("P" + (i + 1) + "\t");
                     for (int j = 0; j < size; j++) {
                         if (j == i)
-                            rem[i] = rem[i] - q;
-                        else if (rem[j] > 0)
-                            wai[j] += q;
+                            tempsCPU1[i] = tempsCPU1[i] - quantum;
+                        else if (tempsCPU1[j] > 0)
+                            waitingT[j] += quantum;
                     }
                 }
                 // calculer le waiting time dans le cas de temps CPU >0 et < quantum
-                else if (rem[i] > 0) {
+                else if (tempsCPU1[i] > 0) {
                     System.out.print("P" + (i + 1) + "\t");
                     for (int j = 0; j < size; j++) {
                         if (j == i)
-                            rem[i] = 0;
-                        else if (rem[j] > 0)
-                            wai[j] += rem[i];
+                            tempsCPU1[i] = 0;
+                        else if (tempsCPU1[j] > 0)
+                            waitingT[j] += tempsCPU1[i];
                     }
                 }
             }
             // calculer le turnaround de chaque processus à partir du waiting time et time CPU
             for (int i = 0; i < size; i++)
-                if (rem[i] > 0)
+                if (tempsCPU1[i] > 0)
                     flag = 1;
 
         } while (flag == 1);
 
         for (int i = 0; i < size; i++)
-            ta[i] = wai[i] + bur[i];
+            teurnaround[i] = waitingT[i] + tempCPU[i];
     }
 
     // l'affichage de tous les processus avec leur waiting time et le turnaround
     void display() {
         System.out.println("\nProcess\tBurst\tWaiting\tTurnaround");
         for (int i = 0; i < size; i++) {
-            System.out.println("P" + (i + 1) + "\t" + bur[i] + "\t" + wai[i] + "\t" + ta[i]);
-            b += wai[i];
-            t += ta[i];
+            System.out.println("P" + (i + 1) + "\t" + tempCPU[i] + "\t" + waitingT[i] + "\t" + teurnaround[i]);
+            b += waitingT[i];
+            t += teurnaround[i];
         }
         System.out.println("Average waiting time:" + (b / size));
         System.out.println("Average Turnaround time:" + (t / size));
